a790147c5f49658df8b76adee27f6f32836670b9
Claire Nord <cnord@mit.edu>
2016-08-10 18:34:52 +0900

    Make navbar responsive

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  //navbar arrow toggle
+  $('#arrow').click( function() {
+    $(this).find('span').toggleClass('glyphicon-chevron-left').toggleClass('glyphicon-chevron-down');
+  });
+
+}());
